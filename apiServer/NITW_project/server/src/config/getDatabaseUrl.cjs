f1c0ca5c4e7f2df0d40bd5024476c7ef6e2dba3a
sebastienjj <josephsebastien.sj@gmail.com>
2022-05-05 11:28:04 -0400

    created an Api for frontend

--- apiServer/NITW_project/server/src/config/getDatabaseUrl.cjs
+++ apiServer/NITW_project/server/src/config/getDatabaseUrl.cjs
@@ -1,9 +1,9 @@
 const getDatabaseUrl = (nodeEnv) => {
   return (
     {
-      development: "postgres://postgres:postgres@localhost:5432/NITW_project_development",
-      test: "postgres://postgres:postgres@localhost:5432/NITW_project_test",
-      e2e: "postgres://postgres:postgres@localhost:5432/NITW_project_e2e",
+      development: "postgres://postgres:postgres@localhost:5444/NITW_project_development",
+      test: "postgres://postgres:postgres@localhost:5444/NITW_project_test",
+      e2e: "postgres://postgres:postgres@localhost:5444/NITW_project_e2e",
     }[nodeEnv] || process.env.DATABASE_URL
   );
 };
--- apiServer/NITW_project/server/src/routes/api/v1/countyRouter.js
+++ apiServer/NITW_project/server/src/routes/api/v1/countyRouter.js
@@ -2,7 +2,10 @@ import express from "express";
 import passport from "passport";
 import { countiesTable } from "../../../models/index.js";
 const countyRouter = new express.Router()
-
+countyRouter.use(function(req, res, next) {
+  res.set("Access-Control-Allow-Origin","http://localhost:3000")
+  next()
+})
 countyRouter.get("/",async (req, res)=> {
   const table = await countiesTable.query()
   console.log(table)
